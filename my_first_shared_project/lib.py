46274757410992da559c1995e5827e57dbc40c8c
Jean Michel YIP <info.test@free.fr>
2022-04-30 14:22:15 +0200

    add lib.py

---
+++ my_first_shared_project/lib.py
@@ -0,0 +1,11 @@
+from termcolor import colored
+
+def who_am_i():
+    print(colored("Hello my name is Gaëtan", "blue"))
+
+def try_me():
+    print(colored("Ok! you've made it to my shared project, Congrat !!!", 'green'))
+
+
+if __name__ == "__main__":
+    try_me()
